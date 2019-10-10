a61cfcf37e5b456cf31815ed0b4e031230fe31b9
SIMONE.PISONI <SIMONE.PISONI@buonarroti.tn.it>
2019-10-10 09:14:14 +0200

    aggiunta commenti

--- java/eserciziopreverifica/src/app/App.java
+++ java/eserciziopreverifica/src/app/App.java
@@ -3,9 +3,13 @@ package app;
 
 
 public class App {
+    //contatore per ciclo
     protected static int cont=0;
+    //semaforo principale per nominatore
     protected static Semaforo s1=new Semaforo(1);
+    //vettore di semafori
     protected static Semaforo[] semafori=new Semaforo[3];
+    //variabile per uccidere tutti i processi
     protected static Boolean alt=true;
     public static void main(String[] args) throws Exception {
         //=======================================================
