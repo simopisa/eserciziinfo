bed36ae8faee895402b0f756516ab88473621485
Simone Pisa <sim1pisoni@gmail.com>
2019-12-19 09:25:50 +0100

    aggiunto qualcosina

--- java/UDP/ServerUDP/src/app/UDPServer.java
+++ java/UDP/ServerUDP/src/app/UDPServer.java
@@ -2,9 +2,10 @@ package app;
 
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 class UDPServer {
-    // ==================================================================================
+    // ======================================================================================================
     DatagramSocket serverSocket;
     byte[] datiricevuti;
     byte[] datidainviare;
@@ -14,19 +15,22 @@ class UDPServer {
     int port;
     String stringadainviare = "";
     DatagramPacket pacchettodainviare;
+    HashMap<String, Integer> Clients;
 
-    // ==================================================================================
-    public UDPServer(int port){
-        this.port=port;
+    // ======================================================================================================
+    public UDPServer(int port) {
+        this.port = port;
+        Clients = new HashMap<String, Integer>();
     }
+
     public void attendi() {
         try {
-            // ==================================================================================
+            // ======================================================================================================
             serverSocket = new DatagramSocket(port);
             datiricevuti = new byte[1024];
             datidainviare = new byte[1024];
             System.out.println("attendo la connesione");
-            // ==================================================================================
+            // ======================================================================================================
         } catch (Exception e) {
             // TODO: handle exception
         }
@@ -36,26 +40,41 @@ class UDPServer {
     public void Comunica() {
         try {
 
-           do {
-                // ==================================================================================
+            do {
+
+                // ======================================================================================================
                 pacchettoricevuto = new DatagramPacket(datiricevuti, datiricevuti.length);
                 serverSocket.receive(pacchettoricevuto);
-                System.out.println("connesso con "+pacchettoricevuto.getAddress()+":"+pacchettoricevuto.getPort());
-                // ==================================================================================
-                stringaricevuta = new String(pacchettoricevuto.getData());
-                System.out.println("<<CLIENT: " + stringaricevuta);
-                // ==================================================================================
-                indirizzoip = pacchettoricevuto.getAddress();
-                port = pacchettoricevuto.getPort();
-                // ==================================================================================
-                stringadainviare = stringaricevuta.toUpperCase();
-                datidainviare = stringadainviare.getBytes();
-                // ==================================================================================
-                pacchettodainviare = new DatagramPacket(datidainviare, datidainviare.length, indirizzoip, port);
-                System.out.println(">>SERVER: "+stringadainviare);
-                serverSocket.send(pacchettodainviare);
-                // ==================================================================================
-           }while (!stringaricevuta.equals("fine"));
+                System.out.println("connesso con " + pacchettoricevuto.getAddress() + ":" + pacchettoricevuto.getPort());
+                if (Clients.get(pacchettoricevuto.getAddress().toString()) == null) {
+
+                    Clients.put(pacchettoricevuto.getAddress().toString(), 1);
+                    System.out.println("nuovo client aggiunto alla lista");
+                } else {
+                    Clients.put(pacchettoricevuto.getAddress().toString(),  Clients.get((pacchettoricevuto.getAddress().toString())) + 1);
+
+                        System.out.println(Clients.get(pacchettoricevuto.getAddress().toString()));
+                }
+                if (Clients.get(pacchettoricevuto.getAddress().toString()) >= 10) {
+                    System.out.println("10 richieste effettuate, ora il servizio è a pagamento");
+                } else {
+                    // ======================================================================================================
+                    stringaricevuta = new String(pacchettoricevuto.getData());
+                    System.out.println("<<CLIENT: " + stringaricevuta);
+                    // ======================================================================================================
+                    indirizzoip = pacchettoricevuto.getAddress();
+                    port = pacchettoricevuto.getPort();
+                    // ======================================================================================================
+                    stringadainviare = stringaricevuta.toUpperCase();
+                    datidainviare = stringadainviare.getBytes();
+                    // ======================================================================================================
+                    pacchettodainviare = new DatagramPacket(datidainviare, datidainviare.length, indirizzoip, port);
+                    System.out.println(">>SERVER: " + stringadainviare);
+                    serverSocket.send(pacchettodainviare);
+                    // ======================================================================================================
+                }
+
+            } while (!stringaricevuta.equals("fine"));
             System.out.println("chiudo la connessione");
             serverSocket.close();
         } catch (Exception e) {
